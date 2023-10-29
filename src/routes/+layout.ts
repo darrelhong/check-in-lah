051227008940e7ea5e43c7eec3b2503d784cb797
darrel.hong <darrel.hong@bytedance.com>
2023-10-29 17:09:58 +0800

    feat: basic landing page

--- src/routes/+layout.ts
+++ src/routes/+layout.ts
@@ -1,1 +1,2 @@
 import 'virtual:uno.css'
+import '@unocss/reset/tailwind.css'
--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,2 +1,44 @@
-<h1>Welcome to SvelteKit</h1>
-<p>Visit <a href="https://kit.svelte.dev">kit.svelte.dev</a> to read the documentation</p>
+<div class="flex flex-col min-h-screen">
+	<div class="flex-1">
+		<div
+			class="absolute inset-x-0 -top-40 -z-10 transform-gpu overflow-hidden blur-3xl sm:-top-80"
+			aria-hidden="true"
+		>
+			<div
+				class="relative left-[calc(50%-11rem)] aspect-[1155/678] w-[36.125rem] -translate-x-1/2 rotate-[30deg] bg-gradient-to-tr from-[#ff80b5] to-[#9089fc] opacity-30 sm:left-[calc(50%-30rem)] sm:w-[72.1875rem]"
+				style="clip-path: polygon(74.1% 44.1%, 100% 61.6%, 97.5% 26.9%, 85.5% 0.1%, 80.7% 2%, 72.5% 32.5%, 60.2% 62.4%, 52.4% 68.1%, 47.5% 58.3%, 45.2% 34.5%, 27.5% 76.7%, 0.1% 64.9%, 17.9% 100%, 27.6% 76.8%, 76.1% 97.7%, 74.1% 44.1%)"
+			/>
+		</div>
+		<header class="m-4">
+			<div class="flex container mx-auto items-center">
+				<p class="font-bold text-blue-600 me-auto text-2xl">check-in-lah</p>
+				<a href="/login" class="font-medium">Login</a>
+			</div>
+		</header>
+		<div class="text-center mt-16 mx-4 mb-24 sm-mb-28">
+			<h1 class="text-4xl sm-text-5xl font-bold mb-6">Fuss-free check ins</h1>
+			<p class="" />
+			<a href="/login" class="text-blue-600 font-medium">Check in now ➜</a>
+		</div>
+
+		<div class="">
+			<div class="max-w-lg mx-auto">
+				<div class="grid gap-8 mx-4">
+					<div class="flex flex-col justify-center">
+						<h2 class="text-2xl font-medium mb-1">What is check-in-lah?</h2>
+						<p class="text-slate-700">
+							check-in-lah is an app that allows you to confirm your check in to facilities with
+							ease.
+						</p>
+					</div>
+					<div class="flex flex-col justify-center">
+						<h2 class="text-2xl font-medium mb-1">How does it work?</h2>
+						<p class="text-slate-700">
+							Simply scan the QR code at the facility or use your current location to verify entry.
+						</p>
+					</div>
+				</div>
+			</div>
+		</div>
+	</div>
+</div>
