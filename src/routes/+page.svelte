0d9fe52f956fdd1fc91ff51341206161255800ab
darrel.hong <darrel.hong@bytedance.com>
2023-11-04 13:35:13 +0800

    feat: update landing page

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,5 +1,6 @@
 <div class="flex flex-col min-h-screen">
 	<div class="flex-1">
+		<!-- background blur bokeh top -->
 		<div
 			class="absolute inset-x-0 -top-40 -z-10 transform-gpu overflow-hidden blur-3xl sm:-top-80"
 			aria-hidden="true"
@@ -9,6 +10,19 @@
 				style="clip-path: polygon(74.1% 44.1%, 100% 61.6%, 97.5% 26.9%, 85.5% 0.1%, 80.7% 2%, 72.5% 32.5%, 60.2% 62.4%, 52.4% 68.1%, 47.5% 58.3%, 45.2% 34.5%, 27.5% 76.7%, 0.1% 64.9%, 17.9% 100%, 27.6% 76.8%, 76.1% 97.7%, 74.1% 44.1%)"
 			/>
 		</div>
+
+		<!-- background blur bokeh top -->
+		<div
+			class="fixed inset-x-0 top-[calc(100%-13rem)] -z-10 transform-gpu overflow-hidden blur-3xl sm:top-[calc(100%-30rem)]"
+			aria-hidden="true"
+		>
+			<div
+				class="relative left-[calc(50%+3rem)] aspect-[1155/678] w-[36.125rem] -translate-x-1/2 bg-gradient-to-tr from-[#ff80b5] to-[#9089fc] opacity-30 sm:left-[calc(50%+36rem)] sm:w-[72.1875rem]"
+				style="clip-path: polygon(74.1% 44.1%, 100% 61.6%, 97.5% 26.9%, 85.5% 0.1%, 80.7% 2%, 72.5% 32.5%, 60.2% 62.4%, 52.4% 68.1%, 47.5% 58.3%, 45.2% 34.5%, 27.5% 76.7%, 0.1% 64.9%, 17.9% 100%, 27.6% 76.8%, 76.1% 97.7%, 74.1% 44.1%)"
+			/>
+		</div>
+
+		<!-- content -->
 		<header class="m-4">
 			<div class="flex container mx-auto items-center">
 				<p class="font-bold text-blue-600 me-auto text-2xl">check-in-lah</p>
@@ -23,16 +37,17 @@
 
 		<div class="">
 			<div class="max-w-lg mx-auto">
-				<div class="grid gap-8 mx-4">
+				<div class="grid gap-4 mx-4">
 					<div class="flex flex-col justify-center">
-						<h2 class="text-2xl font-medium mb-1">What is check-in-lah?</h2>
+						<h2 class="text-xl font-medium mb-1">What is check-in-lah?</h2>
 						<p class="text-slate-700">
 							check-in-lah is an app that allows you to confirm your check in to facilities with
 							ease.
 						</p>
 					</div>
+					<hr class="mx-4" />
 					<div class="flex flex-col justify-center">
-						<h2 class="text-2xl font-medium mb-1">How does it work?</h2>
+						<h2 class="text-xl font-medium mb-1">How does it work?</h2>
 						<p class="text-slate-700">
 							Simply scan the QR code at the facility or use your current location to verify entry.
 						</p>
