ff612de4462021f70525bfb6e325f8a37aa188ce
darrel.hong <darrel.hong@bytedance.com>
2023-11-07 11:04:44 +0800

    feat: update map loading state design

--- src/routes/(app)/home/+page.svelte
+++ src/routes/(app)/home/+page.svelte
@@ -35,7 +35,7 @@
 	});
 
 	const checkInByLocation = async () => {
-		if ((lat === 0 && lng === 0) || locationLoading) {
+		if (locationLoading || (lat === 0 && lng === 0)) {
 			return;
 		}
 		try {
@@ -64,41 +64,42 @@
 				disabled={locationLoading || (lat === 0 && lng === 0)}>Check in</button
 			>
 		</div>
-		{#if locationLoading}
-			<div class="flex items-center">
-				<svg
-					class="animate-spin me-2 h-4 w-4 text-blue-600"
-					xmlns="http://www.w3.org/2000/svg"
-					fill="none"
-					viewBox="0 0 24 24"
-				>
-					<circle
-						class="opacity-25"
-						cx="12"
-						cy="12"
-						r="10"
-						stroke="currentColor"
-						stroke-width="4"
-					/>
-					<path
-						class="opacity-75"
-						fill="currentColor"
-						d="M4 12a8 8 0 018-8V0C5.373 0 0 5.373 0 12h4zm2 5.291A7.962 7.962 0 014 12H0c0 3.042 1.135 5.824 3 7.938l3-2.647z"
-					/>
-				</svg>
-				<p>Waiting for location</p>
-			</div>
-		{/if}
-		{#if lat && lng}
-			<iframe
-				class="rounded-lg shadow max-w-lg w-full h-xs"
-				title="map"
-				src={`https://www.openstreetmap.org/export/embed.html?bbox=${getBoundingBox(
-					lat,
-					lng
-				)}&amp;layer=mapnik&marker=${lat}%2C${lng}`}
-			/>
-			<p class="text-sm text-slate-800">Your current location</p>
-		{/if}
+
+		<div class="rounded-lg shadow max-w-lg w-full h-xs grid place-items-center">
+			{#if locationLoading || (lat === 0 && lng === 0)}
+				<div class="flex items-center">
+					<svg
+						class="animate-spin me-2 h-4 w-4 text-blue-600"
+						xmlns="http://www.w3.org/2000/svg"
+						fill="none"
+						viewBox="0 0 24 24"
+					>
+						<circle
+							class="opacity-25"
+							cx="12"
+							cy="12"
+							r="10"
+							stroke="currentColor"
+							stroke-width="4"
+						/>
+						<path
+							class="opacity-75"
+							fill="currentColor"
+							d="M4 12a8 8 0 018-8V0C5.373 0 0 5.373 0 12h4zm2 5.291A7.962 7.962 0 014 12H0c0 3.042 1.135 5.824 3 7.938l3-2.647z"
+						/>
+					</svg>
+					<p>Waiting for location</p>
+				</div>
+			{:else}
+				<iframe
+					class="w-full h-full"
+					title="map"
+					src={`https://www.openstreetmap.org/export/embed.html?bbox=${getBoundingBox(
+						lat,
+						lng
+					)}&amp;layer=mapnik&marker=${lat}%2C${lng}`}
+				/>
+			{/if}
+		</div>
 	</div>
 </div>
