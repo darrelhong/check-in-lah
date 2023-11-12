47a203cce650ddf7b111c87188077ac4dd998d88
darrel.hong <darrel.hong@bytedance.com>
2023-11-12 22:02:33 +0800

    feat: add sample qr link

--- src/routes/(app)/home/+page.svelte
+++ src/routes/(app)/home/+page.svelte
@@ -1,4 +1,5 @@
 <script lang="ts">
+	import { PUBLIC_SAMPLE_QR_URL } from '$env/static/public';
 	import type { PageData } from './$types';
 	import CheckInByQr from './CheckInByQr.svelte';
 	import CheckInByLocation from './CheckInByLocation.svelte';
@@ -8,11 +9,15 @@
 	$: ({ authStoreModel } = data);
 </script>
 
-<div class="mb-4 flex gap-2 items-center">
+<div class="mb-4 flex flex-col sm:flex-row gap-2 items-start">
 	<p>Logged in as {authStoreModel.email}</p>
 
 	{#if authStoreModel.role === 'admin'}
 		<a class="text-blue-600 font-medium" href="admin">Manage</a>
+	{:else}
+		<a class="text-blue-600 font-medium" href={PUBLIC_SAMPLE_QR_URL} target="_blank"
+			>Sample QR page</a
+		>
 	{/if}
 </div>
 
